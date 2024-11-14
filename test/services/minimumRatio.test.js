6678288642525f12c518d9cd8856950b79ad0cdd
Markus Juuti <marklee@k-id.com>
2024-11-14 18:04:54 +0800

    first attempt

--- test/services/minimumRatio.test.js
+++ test/services/minimumRatio.test.js
@@ -17,45 +17,7 @@ const minCoverageRatioMock = (mocks) => {
 
 describe("services/minCoverageRatio", () => {
   describe("checkMinimumRatio", () => {
-    it("error shouldn't be thrown once feature is not enabled", () => {
-      const minCoverageRatio = NaN;
-      const coverageDiff = 0;
 
-      const getInput = sinon
-        .stub()
-        .withArgs("minimum-ratio")
-        .returns(minCoverageRatio);
-      const setFailed = sinon.spy();
-
-      const checkMinimumRatio = minCoverageRatioMock({
-        getInput,
-        setFailed
-      });
-      const res = checkMinimumRatio(coverageDiff);
-
-      assert.isUndefined(res);
-      assert.isFalse(setFailed.calledOnce);
-    });
-
-    it("error shouldn't be thrown once coverage diff is zero", () => {
-      const minCoverageRatio = 0;
-      const coverageDiff = 0;
-
-      const getInput = sinon
-        .stub()
-        .withArgs("minimum-ratio")
-        .returns(minCoverageRatio);
-      const setFailed = sinon.spy();
-
-      const checkMinimumRatio = minCoverageRatioMock({
-        getInput,
-        setFailed
-      });
-      const res = checkMinimumRatio(coverageDiff);
-
-      assert.isUndefined(res);
-      assert.isFalse(setFailed.calledOnce);
-    });
 
     it("should call setFailed once coverage is less than minimum coverage ratio", () => {
       const minCoverageRatio = 5;
